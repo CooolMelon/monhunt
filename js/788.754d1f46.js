6e72c201ae6d091498096cf4d7a261c42c49e46f
Bob <bobbyeinthavong89@gmail.com>
2023-12-11 14:37:55 +0700

    first commit

---
+++ js/788.754d1f46.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkmonhunt"]=self["webpackChunkmonhunt"]||[]).push([[788],{6788:function(n,u,t){t.r(u),t.d(u,{default:function(){return f}});var a=t(66252);const e={class:"about"},r=(0,a._)("h1",null,"This is an about page",-1),s=[r];function c(n,u){return(0,a.wg)(),(0,a.iD)("div",e,s)}var o=t(83744);const i={},h=(0,o.Z)(i,[["render",c]]);var f=h}}]);
+//# sourceMappingURL=788.754d1f46.js.map
